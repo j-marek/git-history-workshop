ed9b009e27f45293ca56f43e830ed35373aca0bc
Jan Marek <jan.marek@orbit.cz>
2021-02-15 14:52:00 +0100

    Fix browser compatibility

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:24:54 CEST 2025
 // Update Mon Jul 28 09:24:54 CEST 2025
 // Update Mon Jul 28 09:24:54 CEST 2025
+// Update Mon Jul 28 09:24:55 CEST 2025
