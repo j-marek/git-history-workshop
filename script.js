863235604813cbf7d87bc2a11dd6d7fc22bf9de1
Jan Marek <jan.marek@orbit.cz>
2021-03-11 17:07:00 +0100

    Add debugging tools

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:24:55 CEST 2025
 // Update Mon Jul 28 09:24:55 CEST 2025
 // Update Mon Jul 28 09:24:55 CEST 2025
+// Update Mon Jul 28 09:24:55 CEST 2025
