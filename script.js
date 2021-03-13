6391e5723b191f45a23c6bd10056c4b5f6783289
Jan Marek <jan.marek@orbit.cz>
2021-03-13 12:24:00 +0100

    Add user interactions

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:24:55 CEST 2025
 // Update Mon Jul 28 09:24:55 CEST 2025
 // Update Mon Jul 28 09:24:55 CEST 2025
+// Update Mon Jul 28 09:24:55 CEST 2025
