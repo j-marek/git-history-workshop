6cd0aab3630811406c123bfe97eafc7202a58020
Jan Marek <jan.marek@orbit.cz>
2021-03-27 17:09:00 +0100

    Fix memory leaks

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:24:55 CEST 2025
 // Update Mon Jul 28 09:24:55 CEST 2025
 // Update Mon Jul 28 09:24:55 CEST 2025
+// Update Mon Jul 28 09:24:55 CEST 2025
