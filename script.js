021cd4eb767f01227ce41b688d520e800763adcd
Jan Marek <jan.marek@orbit.cz>
2021-12-23 17:36:00 +0100

    Add new features

--- script.js
+++ script.js
@@ -38,3 +38,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:24:59 CEST 2025
 // Update Mon Jul 28 09:24:59 CEST 2025
 // Update Mon Jul 28 09:25:00 CEST 2025
+// Update Mon Jul 28 09:25:00 CEST 2025
