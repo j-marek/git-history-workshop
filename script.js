c51ccf2ad8762236bb4732ce1084ac040e958462
Jan Marek <jan.marek@orbit.cz>
2023-03-15 11:00:00 +0100

    Update event handlers

--- script.js
+++ script.js
@@ -92,3 +92,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:25:07 CEST 2025
 // Update Mon Jul 28 09:25:07 CEST 2025
 // Update Mon Jul 28 09:25:07 CEST 2025
+// Update Mon Jul 28 09:25:07 CEST 2025
