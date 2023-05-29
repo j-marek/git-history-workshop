2940dceb80470166d0f0b8352aedf5fd41ba71ce
Jan Marek <jan.marek@orbit.cz>
2023-05-29 13:01:00 +0200

    Update API calls

--- script.js
+++ script.js
@@ -101,3 +101,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:25:08 CEST 2025
 // Update Mon Jul 28 09:25:08 CEST 2025
 // Update Mon Jul 28 09:25:08 CEST 2025
+// Update Mon Jul 28 09:25:09 CEST 2025
