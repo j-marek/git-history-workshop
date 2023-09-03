7921cafd35d425a4554cbd98247b1162afafead2
Jan Marek <jan.marek@orbit.cz>
2023-09-03 11:06:00 +0200

    Improve performance

--- script.js
+++ script.js
@@ -111,3 +111,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:25:10 CEST 2025
 // Update Mon Jul 28 09:25:10 CEST 2025
 // Update Mon Jul 28 09:25:10 CEST 2025
+// Update Mon Jul 28 09:25:10 CEST 2025
