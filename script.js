825056b3a8ce9a1a88de6bd55b855b1211b1803a
Jan Marek <jan.marek@orbit.cz>
2025-02-09 11:05:00 +0100

    Add form validation

--- script.js
+++ script.js
@@ -170,3 +170,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:25:19 CEST 2025
 // Update Mon Jul 28 09:25:19 CEST 2025
 // Update Mon Jul 28 09:25:19 CEST 2025
+// Update Mon Jul 28 09:25:19 CEST 2025
