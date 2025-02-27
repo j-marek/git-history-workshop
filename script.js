1ba323b1c2ccd9808344bb78de23b8197a4d1596
Jan Marek <jan.marek@orbit.cz>
2025-02-27 15:53:00 +0100

    Fix async issues

--- script.js
+++ script.js
@@ -173,3 +173,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:25:19 CEST 2025
 // Update Mon Jul 28 09:25:20 CEST 2025
 // Update Mon Jul 28 09:25:20 CEST 2025
+// Update Mon Jul 28 09:25:20 CEST 2025
