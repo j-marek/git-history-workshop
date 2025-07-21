3bbdddb02840ccfd77f705ef97e10a4078fc5a46
Jan Marek <jan.marek@orbit.cz>
2025-07-21 11:26:00 +0200

    Improve code structure

--- script.js
+++ script.js
@@ -192,3 +192,4 @@ console.log('Website loaded');
 // Update Mon Jul 28 09:25:21 CEST 2025
 // Update Mon Jul 28 09:25:22 CEST 2025
 // Update Mon Jul 28 09:25:22 CEST 2025
+// Update Mon Jul 28 09:25:22 CEST 2025
